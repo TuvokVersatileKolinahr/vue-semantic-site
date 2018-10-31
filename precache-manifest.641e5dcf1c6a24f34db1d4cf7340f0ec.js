c262f69df942ec45cde75b17132b0ff54c51200b
Wouter <tuvokki@gmail.com>
2018-10-31 11:05:19 +0100

    9fb0645

---
+++ precache-manifest.641e5dcf1c6a24f34db1d4cf7340f0ec.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/vue-semantic-site/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "62551504ad124c62b74f",
+    "url": "/vue-semantic-site/js/about.93982010.js"
+  },
+  {
+    "revision": "84caa293a6f7c35c109f",
+    "url": "/vue-semantic-site/js/app.6ddfc54c.js"
+  },
+  {
+    "revision": "0f17fb5d295ea514559d",
+    "url": "/vue-semantic-site/js/chunk-vendors.2fcfc143.js"
+  },
+  {
+    "revision": "054dd1f08c196c104433",
+    "url": "/vue-semantic-site/js/profile.0a144843.js"
+  },
+  {
+    "revision": "1f4d2f4de019e2836adb",
+    "url": "/vue-semantic-site/js/login.385385e3.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/vue-semantic-site/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9cf18c73c140b78fc5838062ff4e67aa",
+    "url": "/vue-semantic-site/index.html"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/vue-semantic-site/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "3f377e7647fa385b0e699cd64a4978c8",
+    "url": "/vue-semantic-site/img/logo.3f377e76.png"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/vue-semantic-site/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/vue-semantic-site/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/vue-semantic-site/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "5d6ef2929b938c5caebc670fb6685161",
+    "url": "/vue-semantic-site/img/Bridge_Tuvok.5d6ef292.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/vue-semantic-site/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/vue-semantic-site/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/vue-semantic-site/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/vue-semantic-site/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/vue-semantic-site/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/vue-semantic-site/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/vue-semantic-site/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/vue-semantic-site/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/vue-semantic-site/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/vue-semantic-site/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "054dd1f08c196c104433",
+    "url": "/vue-semantic-site/css/profile.444af74b.css"
+  },
+  {
+    "revision": "1f4d2f4de019e2836adb",
+    "url": "/vue-semantic-site/css/login.0e433876.css"
+  },
+  {
+    "revision": "84caa293a6f7c35c109f",
+    "url": "/vue-semantic-site/css/app.79f94534.css"
+  }
+];
